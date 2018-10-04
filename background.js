4e878254ede6dcfe585bb6d0fd492574cada501a
supritkumar shah <suprit7@gmail.com>
2018-10-03 21:27:50 -0400

    fully working extension

---
+++ background.js
@@ -0,0 +1,18 @@
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+    // Send a message to the active tab
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      var activeTab = tabs[0];
+      // sends message to scripts.js file
+      // activeTab contains all the details about the current active tab. Like url and other metadata
+      chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action","url":activeTab});
+    });
+  });
+  
+  chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      if( request.message === "open_new_tab" ) {
+        chrome.tabs.create({"url": request.url});
+      }
+    }
+  );
---
+++ script.js
@@ -0,0 +1,25 @@
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        // receives message from background.js
+        if( request.message === 'clicked_browser_action') {
+            var query = request.url.url;
+            var url = "MY_API"+query; // My API is hidden as I have not implemented security to my end point
+            fetch(url)
+                .then(function(response) {
+                    if (!response.ok) {
+                        throw Error(response.statusText);
+                    }
+                    // Read the response as json.
+                    return response.json();
+                    })
+                    .then(function(responseAsJson) {
+                    // parse json for URL and respond back with stream url
+                        var streamUrl = responseAsJson.info.formats[1].url;            
+                        chrome.runtime.sendMessage({"message": "open_new_tab", "url": streamUrl});
+                    })
+                    .catch(function(error) {
+                    console.log('Looks like there was a problem: \n', error);
+                });
+        }
+    }
+);
